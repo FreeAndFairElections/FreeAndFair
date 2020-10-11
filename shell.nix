b8b80b437a2aea6e94b7c8337da5e0d457f046aa
Dave Nicponski <dave.nicponski@gmail.com>
2020-10-11 09:50:39 -0400

    Nixified dev environment
    We like deterministic build environments!

---
+++ shell.nix
@@ -0,0 +1,40 @@
+{ pkgs ? import ./nixpkgs.nix { config = {}; overlays = []; } }:
+
+with pkgs;
+
+let
+ project_name = "FreeAndFair";
+
+in mkShell {
+  buildInputs =
+    let
+      coreInputs = [
+        bazel_3
+        bazel-buildtools
+        cacert
+        coreutils
+        gcc
+        gitFull
+        nix
+        python3
+        which
+      ];
+
+      scalaInputs = [
+        openjdk8
+      ] ++ lib.optionals stdenv.isDarwin [
+        (xcbuild.override { sdkVer = "10.15"; })
+      ];
+
+      jsInputs = [
+        nodejs
+      ];
+    in
+      coreInputs ++ scalaInputs ++ jsInputs;
+
+  name = "${project_name}-build-nix-shell";
+
+  shellHook = ''
+    echo >&2 "Entered the '${project_name}' nixified development (and bazel execution) environment"
+  '';
+}
